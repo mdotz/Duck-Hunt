181630fd43b2872d9399641d2d35ff71d09c3f57
Marcin Zieliński <marcin.x.zielinski@gmail.com>
2018-01-04 23:10:30 +0100

    background, canvas position/resizing

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 $(document).ready( function(){
   const container = $('#container');
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
 
   let width;
   let height;
 
   updateViewportSize();
 
-  console.log(container.css('background-image'));
-
   $(window).resize(function(){
     updateViewportSize();
   });
 
+
+
+
+
   function updateViewportSize(){
     width = $(window).width();
     height = $(window).height();
-
 
     container.css('background-image',"url(background.png)")
     if(width >= 854 && height >= 480) {
@@ -24,5 +27,10 @@ $(document).ready( function(){
     else {
       container.css('background-size', `854px 480px`);
     }
+
+    canvas.height = height/1.61;
+    canvas.width = canvas.height;
+    context.fillStyle = 'yellow';
+    context.fillRect(0, 0, canvas.width, canvas.height);
   }
 });
